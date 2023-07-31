7e909b3c7ca9b9a3507ec68a36cf045bb4131081
bethanyyy <btyao2@illinois.edu>
2023-07-31 14:50:42 -0400

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,15 @@ import { theme } from "../themes/theme";
 import { ThemeProvider } from "styled-components";
 import { AnimatePresence } from "framer-motion";
 
+import { Analytics } from "@vercel/analytics/react";
+
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <AnimatePresence mode="wait" initial={true}>
           <Component {...pageProps} key={router.pathname} />
+          <Analytics />
         </AnimatePresence>
       </Layout>
     </ThemeProvider>
